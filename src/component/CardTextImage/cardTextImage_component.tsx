9db63dbf0ec982a0329e5c9e7bfecae8195a6d49
Robert Ramirez <78374181+robertramirezmarin@users.noreply.github.com>
2021-04-20 17:51:05 -0600

    P-104: FurtherEducation Card modal

--- src/component/CardTextImage/cardTextImage_component.tsx
+++ src/component/CardTextImage/cardTextImage_component.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
 import "./cardTextImage_component.scss";
 
 type TProps = {
   image: string;
   title: string;
-  link: string;
+  url: string;
+  description: string;
+  date: string;
 };
 
-const CardTextImage = ({ image, title }: TProps) => {
+const CardTextImage = ({ image, title, url, description, date }: TProps) => {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <div className="CardTextImage">
+    <div
+      className="CardTextImage"
+      onClick={() => {
+        console.log("card");
+        if (openModal) {
+          setOpenModal(!openModal);
+        }
+        setOpenModal(!openModal);
+      }}
+    >
       <div className="CardTextImage__imageContainer">
         <img src={image} alt="Card Image" className="" />
       </div>
       <div className="CardTextImage__textContainer">
-        <h2 className="title">{title}</h2>
+        <p className="subtitle CardTextImage__title">{title}</p>
+        <p className="body-regular">{description}</p>
+        <p className="body-regular">{date}</p>
+      </div>
+      <div
+        onClick={() => setOpenModal(true)}
+        className={`CardTextImage__modalBackground${
+          openModal ? "Open" : "Closed"
+        }`}
+      />
+      <div className="CardTextImage__modal">
+        <h1>a</h1>
       </div>
     </div>
   );
--- src/modules/About/FurtherEducation/furtherEducation_component.tsx
+++ src/modules/About/FurtherEducation/furtherEducation_component.tsx
@@ -2,15 +2,42 @@ import CardTextImage from "@/src/component/CardTextImage/cardTextImage_component
 import React from "react";
 import "./furtherEducation_component.scss";
 
+const FurtherEducationItems = [
+  {
+    image: "assets/home/HomeCard1.jpg",
+    title: "First Place Evolution Ideas",
+    description: "Design Thiniking competition",
+    date: "December 2019",
+    url: "/",
+  },
+  {
+    image: "assets/home/HomeCard1.jpg",
+    title: "IBM Experience",
+    description: "IBM Mentoring Program",
+    date: "December 2019",
+    url: "/",
+  },
+];
+
 const FurtherEducation = () => {
   return (
     <div className="FurtherEducation section">
       <div className="grid">
         <div className="col-desk-12 col-mob-4">
-          <CardTextImage
-            image="assets/home/HomeCard1.jpg"
-            title="First Place Evolution Ideas"
-          />
+          <h1 className="title">Further Education</h1>
+          <div className="FurtherEducation__cardsContainer">
+            {FurtherEducationItems.map((item, index) => (
+              <div key={index}>
+                <CardTextImage
+                  image={item.image}
+                  title={item.title}
+                  description={item.description}
+                  date={item.date}
+                  url={item.url}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
