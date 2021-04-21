64f8fdcca5ed316239583e9384f2e425b38ec098
Robert Ramirez <78374181+robertramirezmarin@users.noreply.github.com>
2021-04-21 12:08:19 -0600

    P-104: Modal mobile fix

--- src/modules/About/FurtherEducation/ModalChildrens/DesignThinking.tsx
+++ src/modules/About/FurtherEducation/ModalChildrens/DesignThinking.tsx
@@ -6,14 +6,27 @@ const DesignThinking = ({ title, description, date }) => {
       <h1 className="subtitle">{title}</h1>
       <p className="body-regular">{description}</p>
       <p className="body-regular">{date}</p>
-
       <video controls className="FurtherEducation__iframe">
         <source
           src="https://portfolio-robertram.s3.amazonaws.com/FurtherTraining/WhatsApp+Video+2019-12-08+at+5.24.51+PM.mp4"
           type="video/mp4"
         />
       </video>
-
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nostrum nam
+        delectus omnis dolorum laborum! Eos necessitatibus porro saepe ullam,
+        quos, odio explicabo quam quod suscipit nesciunt odit aut omnis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nostrum nam
+        delectus omnis dolorum laborum! Eos necessitatibus porro saepe ullam,
+        quos, odio explicabo quam quod suscipit nesciunt odit aut omnis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nostrum nam
+        delectus omnis dolorum laborum! Eos necessitatibus porro saepe ullam,
+        quos, odio explicabo quam quod suscipit nesciunt odit aut omnis!
+      </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nostrum nam
         delectus omnis dolorum laborum! Eos necessitatibus porro saepe ullam,
